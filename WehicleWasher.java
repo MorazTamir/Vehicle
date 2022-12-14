e20f4503bf837ddb4dd6003033de7377f7f22e46
Moraz_Tamir <morazt2@gmail.com>
2022-12-14 15:04:42 +0200

    Second commit point

--- WehicleWasher.java
+++ WehicleWasher.java
@@ -1,6 +1,7 @@
 //Author1: Ninel Benush 324699826 ,Author2: Moraz Tamir 208397455
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class WehicleWasher {
 	int n;
@@ -25,16 +26,32 @@ public class WehicleWasher {
 		this.initialTime = System.currentTimeMillis();
 	}
 
-	public synchronized void wash(Wehicle w) throws InterruptedException {
+	public void wash(Wehicle w) throws InterruptedException {
+		arriveCar(w);
+		synchronized (this) {
+			while (washing.size() == n)
+				wait();
+		}
+		washCar(w);
+		finishWash(w);
+		synchronized (this) {
+			notifyAll();
+		}
+	}
+
+	public void arriveCar(Wehicle w) {
 		waiting.add(w);
 		printStatus("new vehicle arrived", w);
-		while (washing.size() == n)
-			w.wait();
+	}
+
+	public void washCar(Wehicle w) throws InterruptedException {
 		printStatus("The vehicle got into on of the stations", w);
 		washing.add(w);
 		waiting.remove(w);
-		int delay = (int) (avgWash * 1000);
-		Thread.sleep(delay);
+		Thread.sleep(calcNextTime());
+	}
+
+	public void finishWash(Wehicle w) {
 		if (w instanceof Car)
 			car.add((Car) w);
 		else if (w instanceof SUV)
@@ -45,14 +62,19 @@ public class WehicleWasher {
 			minibus.add((MiniBus) w);
 		printStatus("The vehicle finish to wash himself", w);
 		washing.remove(w);
-		this.notifyAll();
-
 	}
 
-	public void printStatus(String message, Wehicle w) {
+	public int calcNextTime() {
+		Random rand = new Random();
+		double u = rand.nextDouble();
+		double nextTime = (-(Math.log(u) / avgWash)) * 1000;
+		return (int) nextTime;
+	}
+
+	public synchronized void printStatus(String message, Wehicle w) {
 		long currentTime = System.currentTimeMillis() - initialTime;
 		System.out.println("---------------------");
-		System.out.println("Time from the start: " + currentTime);
+		System.out.println("Time from the start: " + currentTime + " milli seconds");
 		System.out.println("Status: " + message);
 		System.out.println("Id: " + w.id + ", Type: " + w.type);
 	}
